9015892ca062c0f0d21e603f8c3c8df33c5c673d
kidd-cn <xiezhide@gmail.com>
2026-05-22 22:49:17 +0800

    feat: add details

---
+++ miniprogram/pages/activities/activity-center.js
@@ -0,0 +1,43 @@
+// pages/activities/activity-center.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 跳转到识别挑战页面
+   */
+  goToIdentificationGame() {
+    wx.navigateTo({
+      url: './identification-game'
+    });
+  },
+
+  /**
+   * 跳转到知识问答页面
+   */
+  goToKnowledgeQuiz() {
+    wx.navigateTo({
+      url: './knowledge-quiz'
+    });
+  },
+
+  /**
+   * 跳转到每日签到页面
+   */
+  goToCheckIn() {
+    wx.navigateTo({
+      url: './check-in'
+    });
+  }
+});
