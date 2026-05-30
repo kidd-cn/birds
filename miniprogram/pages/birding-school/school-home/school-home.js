27989a17f73ab303f619fa577f4a5e550c02f702
kidd-cn <xiezhide@gmail.com>
2026-05-30 18:21:09 +0800

    feat: add school-home page for birding school

---
+++ miniprogram/pages/birding-school/school-home/school-home.js
@@ -0,0 +1,58 @@
+const schoolData = require('../../../utils/school-data');
+
+Page({
+  data: {
+    topics: [],
+    totalCompleted: 0,
+    totalTopics: 0
+  },
+
+  onLoad() {
+    this.loadTopics();
+  },
+
+  onShow() {
+    this.loadTopics();
+  },
+
+  loadTopics() {
+    const topics = schoolData.getTopics();
+    const completed = topics.filter(t => t.status === 'completed').length;
+
+    this.setData({
+      topics: topics.map(t => ({
+        ...t,
+        position: this.getTopicPosition(t.order)
+      })),
+      totalCompleted: completed,
+      totalTopics: topics.length
+    });
+  },
+
+  getTopicPosition(order) {
+    const positions = [
+      { top: 80, left: 60 },
+      { top: 280, left: 280 },
+      { top: 480, left: 60 },
+      { top: 680, left: 280 }
+    ];
+    return positions[order - 1] || { top: 80, left: 60 };
+  },
+
+  onTopicTap(e) {
+    const { topicId } = e.detail;
+    const topic = this.data.topics.find(t => t.id === topicId);
+
+    if (topic.status === 'locked') {
+      wx.showToast({
+        title: '请先完成前一主题',
+        icon: 'none'
+      });
+      return;
+    }
+
+    wx.navigateTo({
+      url: `/pages/birding-school/topic-detail/topic-detail?topicId=${topicId}`
+    });
+  }
+});
